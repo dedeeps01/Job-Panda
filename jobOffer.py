dbfcb3a63cf2ccd43a4b40ef74b70c3fb68c7f13
Cai Castillo <53364022+cai-ccc@users.noreply.github.com>
2022-02-19 18:26:28 -0500

    changed salary.txt for just quickResources for easy information access; changed jobOffer to more descriptive name and added possible tokenizer funtions for testing

--- jobOffer.py
+++
@@ -1,8 +0,0 @@
-# the goal of this is to upload a job offer letter, scan for position, location, 401k/Roth IRA, 
-# and return a quickview summary. 
-import nltk
-import PyPDF2
-
-# extracting text from pdf files 
-JobOfferPDF = open('example.pdf', 'rb')
-
